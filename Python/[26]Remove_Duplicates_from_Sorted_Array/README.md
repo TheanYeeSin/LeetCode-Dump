ab06bbcab2ed421d2ed29f58888f5fc9c5a44848
Thean Yee Sin <yeesinthean@outlook.com>
2024-04-29 07:18:39 +0800

    Add 26. Remove Duplicates from Sorted Array

---
+++ Python/[26]Remove_Duplicates_from_Sorted_Array/README.md
@@ -0,0 +1,14 @@
+
+# Remove_Duplicates_from_Sorted_Array
+
+Difficulty: [EDIT HERE]
+
+# Question Description
+
+[EDIT HERE]
+
+# Solution
+
+[Remove_Duplicates_from_Sorted_Array]([26]Remove_Duplicates_from_Sorted_Array.py)
+    
+    
---
+++ Python/[26]Remove_Duplicates_from_Sorted_Array/[26]Remove_Duplicates_from_Sorted_Array.py
@@ -0,0 +1,13 @@
+# Your Python code goes here.
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        k = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[k] = nums[i]
+                k += 1
+        return k
