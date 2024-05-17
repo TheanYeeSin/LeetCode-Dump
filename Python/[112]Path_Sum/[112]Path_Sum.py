511aa2bb837f33b9b854551e687376f4538a6ee1
Thean Yee Sin <yeesinthean@outlook.com>
2024-05-17 09:01:36 +0800

    Add 112. Path Sum

---
+++ Python/[112]Path_Sum/[112]Path_Sum.py
@@ -0,0 +1,24 @@
+# Your Python code goes here.
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def hasPathSum(self, root, targetSum):
+        """
+        :type root: TreeNode
+        :type targetSum: int
+        :rtype: bool
+        """
+        if not root:
+            return None
+        targetSum -= root.val
+        if targetSum == 0 and not root.left and not root.right:
+            return True
+        return self.hasPathSum(root.left, targetSum) or self.hasPathSum(
+            root.right, targetSum
+        )
