594b03ace80f3b4dc01f73c1ed0e58d1a79d0481
Thean Yee Sin <yeesinthean@outlook.com>
2024-06-23 17:31:08 +0800

    Add 242. Valid Anagram

---
+++ Python/[242]Valid_Anagram/[242]Valid_Anagram.py
@@ -0,0 +1,6 @@
+# Your Python code goes here.
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s = "".join(sorted(s))
+        t = "".join(sorted(t))
+        return s == t
