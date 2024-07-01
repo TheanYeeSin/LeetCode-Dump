5cf38c26d720485073ae8da4a3703a803dc6d9f6
Thean Yee Sin <yeesinthean@outlook.com>
2024-07-01 09:14:49 +0800

    Add 290. Word Pattern

---
+++ Python/[290]Word_Pattern/[290]Word_Pattern.py
@@ -0,0 +1,6 @@
+# Your Python code goes here.
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        s = s.split()
+
+        return len(set(pattern)) == len(set(s)) == len(set(zip_longest(pattern, s)))
