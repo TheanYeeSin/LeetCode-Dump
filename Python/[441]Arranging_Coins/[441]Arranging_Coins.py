99c7d2d0d397c59d69413e883bdf2719f5c46876
Thean Yee Sin <yeesinthean@outlook.com>
2024-07-21 20:14:50 +0800

    Add 441. Arranging Coins

---
+++ Python/[441]Arranging_Coins/[441]Arranging_Coins.py
@@ -0,0 +1,8 @@
+# Your Python code goes here.
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        row = 0
+        while n >= row + 1:
+            row += 1
+            n -= row
+        return row
