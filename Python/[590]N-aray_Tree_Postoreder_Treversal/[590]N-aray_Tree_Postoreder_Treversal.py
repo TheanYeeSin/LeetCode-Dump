24d9721199069da526f76938cab7d08afd5b82a8
Thean Yee Sin <yeesinthean@outlook.com>
2024-08-13 09:00:40 +0800

    Add 590. N-ary Tree Postorder Traversal

---
+++ Python/[590]N-aray_Tree_Postoreder_Treversal/[590]N-aray_Tree_Postoreder_Treversal.py
@@ -0,0 +1,23 @@
+# Your Python code goes here.
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+
+class Solution:
+    def postorder(self, root: "Node") -> List[int]:
+        arr = []
+
+        def order(root):
+            if root is None:
+                return None
+            for i in root.children:
+                order(i)
+            arr.append(root.val)
+
+        order(root)
+        return arr
