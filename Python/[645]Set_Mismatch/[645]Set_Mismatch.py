3b8a5dfa755bf1e3481f80fce31d686a1ef51006
Thean Yee Sin <yeesinthean@outlook.com>
2024-08-29 10:14:38 +0800

    Add 645. Set Mismatch

---
+++ Python/[645]Set_Mismatch/[645]Set_Mismatch.py
@@ -0,0 +1,9 @@
+# Your Python code goes here.
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        n, memo = len(nums), set()
+
+        for num in nums:
+            if num in memo:
+                return [num, (n * (n + 1) // 2) - sum(nums) + num]
+            memo.add(num)
