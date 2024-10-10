3963dfcc3daa51b3c9996d3003ef915d6bacfc4c
Thean Yee Sin <yeesinthean@outlook.com>
2024-10-10 10:02:38 +0800

    Add 888. Fair Candy Swap

---
+++ Python/[888]Fair_Candy_Swap/[888]Fair_Candy_Swap.py
@@ -0,0 +1,8 @@
+# Your Python code goes here.
+class Solution:
+    def fairCandySwap(self, aliceSizes: List[int], bobSizes: List[int]) -> List[int]:
+        delta = (sum(aliceSizes) - sum(bobSizes)) // 2
+        aliceSizes = set(aliceSizes)
+        for size in set(bobSizes):
+            if delta + size in aliceSizes:
+                return [delta + size, size]
