df36491cdbd04aa66741ee191ce9673d0ddff509
Thean Yee Sin <yeesinthean@outlook.com>
2024-11-01 10:13:42 +0800

    Add 989. Add to Array-Form of Integer

---
+++ Python/[989]Add_to_Array-Form_of_Integer/[989]Add_to_Array-Form_of_Integer.py
@@ -0,0 +1,14 @@
+# Your Python code goes here.
+class Solution(object):
+    def addToArrayForm(self, num, k):
+        """
+        :type num: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        for i in range(len(num) - 1, -1, -1):
+            k, num[i] = divmod(num[i] + k, 10)
+        while k:
+            k, a = divmod(k, 10)
+            num = [a] + num
+        return num
