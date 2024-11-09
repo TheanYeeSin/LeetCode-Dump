6376df04bf270fb5f2939683c022fe6659fbb25c
Thean Yee Sin <yeesinthean@outlook.com>
2024-11-09 14:10:31 +0800

    Add 1009. Complement of Base 10 Integer

---
+++ Python/[1009]Complement_of_Base_10_Integer/[1009]Complement_of_Base_10_Integer.py
@@ -0,0 +1,8 @@
+# Your Python code goes here.
+class Solution(object):
+    def bitwiseComplement(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return ((2 << int(math.log(max(n, 1), 2))) - 1) - n
