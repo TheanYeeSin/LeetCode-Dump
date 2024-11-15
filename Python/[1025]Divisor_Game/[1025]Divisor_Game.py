04a469e89c6f51a0eb6cc21fa00eef65e742731b
Thean Yee Sin <yeesinthean@outlook.com>
2024-11-15 10:01:55 +0800

    Add 1025. Divisor Game

---
+++ Python/[1025]Divisor_Game/[1025]Divisor_Game.py
@@ -0,0 +1,8 @@
+# Your Python code goes here.
+class Solution(object):
+    def divisorGame(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return n % 2 == 0
