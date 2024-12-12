4c20c3e30337d6ad4c600a0d5a61e9eb69d144d3
Thean Yee Sin <yeesinthean@outlook.com>
2024-12-12 13:58:29 +0800

    Add 1137. N-th Tribonacci Number

---
+++ Python/[1137]Nth_Tribonacci_Number/[1137]Nth_Tribonacci_Number.py
@@ -0,0 +1,17 @@
+# Your Python code goes here.
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        a = 0
+        b = 1
+        c = 1
+        ans = 0
+        if n == 0 or n == 1:
+            return n
+        if n == 2:
+            return 1
+        for i in range(3, n + 1):
+            ans = a + b + c
+            a = b
+            b = c
+            c = ans
+        return ans
